d680b5daaf705a79be9112430836e1ad2d79f520
Gadiel Petronilo <gpetronilo@pdmfc.com>
2024-07-02 22:31:22 -0300

    Seed updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::table('item_groups')->insert([
+            'name' => 'Item',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('items')->insert([
+            'name' => 'Fita Crepe 19x50MM',
+            'item_group_id' => 1,
+            'price' => 4730,
+            'sale_price' => 4998,
+            'iventory_item' => true,
+            'sale_item' => true,
+            'purchase_item' => false,
+            'measurement_unit_id' => 1,
+        ]);
+
+        DB::table('measurement_units')->insert([
+            'name' => 'Quantidade',
+            'abbreviation' => 'Qtd',
+        ]);
+
+        DB::table('inventories')->insert([
+            'item_id' => 1,
+            'quantity' => 50,
         ]);
     }
 }
